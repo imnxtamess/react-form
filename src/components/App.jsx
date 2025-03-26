ea2bb50ef089404de136d5f7aa7accd174895a5a
imnxtamess <davide.morisco.work@gmail.com>
2025-03-26 13:52:21 +0100

    Implement article submission functionality and update form handling in App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,23 @@
 import { useState } from "react";
+const articlesList = [
+  "Article1",
+  "Article2",
+  "Article3",
+  "Article4",
+  "Article5",
+];
 export default function App() {
-  const articles = ["Article1", "Article2", "Article3", "Article4", "Article5"];
+  const [newArticle, setNewArticle] = useState("");
+  const [articles, setArticles] = useState(articlesList);
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    console.log(newArticle);
+    setArticles([...articles, newArticle]);
+    console.log(articles);
+    setNewArticle("");
+  }
+
   return (
     <>
       <div className="container">
@@ -16,8 +33,9 @@ export default function App() {
             </li>
           ))}
         </ul>
-        <form className="mt-5 row">
+        <form className="mt-5 row" onSubmit={handleFormSubmit}>
           <div className="col-10">
+            <label htmlFor="newAritcle">Article</label>
             <input
               type="text"
               className="form-control"
@@ -25,6 +43,10 @@ export default function App() {
               id="newArticle"
               aria-describedby="article_helper"
               placeholder="Add a new Article"
+              value={newArticle}
+              onChange={(e) => {
+                setNewArticle(e.target.value);
+              }}
             />
             <small id="article_helper" className="form-text text-muted">
               Type the article title above
@@ -32,7 +54,7 @@ export default function App() {
           </div>
           <button
             type="submit"
-            className="btn btn-primary col-2 align-self-start"
+            className="btn btn-primary col-2 align-self-center"
           >
             Submit
           </button>
